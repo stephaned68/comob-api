67db1f64da756f5d834a75974a22334617e1213c
Stéphane Devouard <sdevouard@gmail.com>
2019-07-06 13:09:58 +0200

    Added cors package and cors middleware to the app

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
 const datasetRoutes = require('./routes/datasets');
 const profilRoutes = require('./routes/profils');
 const voieRoutes = require('./routes/voies');
 const capaciteRoutes = require('./routes/capacites');
 
+app.use(cors());
 app.use('/datasets', datasetRoutes);
 app.use('/profils', profilRoutes);
 app.use('/voies', voieRoutes);
