8f0a6f2aa9a9ff4e337cdd68711771ae2eda223f
Stéphane Devouard <sdevouard@gmail.com>
2019-09-01 16:47:18 +0200

    Added support for port=NNNNN on the command line to run the server

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const http = require('http');
 const app = require('./app');
 
+let usePort;
+for (let argv of process.argv) {
+  if (argv.toLowerCase().startsWith('port=')) {
+    usePort = argv.split('=')[1];
+  }
+}
+
 const ip = process.env.IP || '127.0.0.1';
-const port = process.env.PORT || 8100;
+const port = process.env.PORT || (usePort || 8100);
 
 const server = http.createServer(app);
 
