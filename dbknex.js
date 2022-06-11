3b0f22298c4bc2010e0dfafe9a5183ce6bae1987
Stéphane Devouard <sdevouard@gmail.com>
2022-06-11 17:42:38 +0200

    Protect db secrets

--- dbknex.js
+++ dbknex.js
@@ -1,9 +1,18 @@
 const config = require('config');
 const trace = require('./trace');
 
+let dbConfig = config.get('db');
+if (dbConfig === {}) {
+  dbConfig.host = process.env.DB_HOST;
+  dbConfig.port = process.env.DB_PORT;
+  dbConfig.user = process.env.DB_USER;
+  dbConfig.password = process.env.DB_PASS;
+  dbConfig.database = process.env.DB_NAME;
+}
+
 const knex = require('knex')({
   client: 'mysql',
-  connection: config.get('db'),
+  connection: dbConfig,
 });
 
 knex.on('query', function (debugData) {
