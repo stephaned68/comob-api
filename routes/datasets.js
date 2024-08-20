6a089d5433cd57e1ce6dc92ffb4cc02307d0ab33
Stéphane Devouard <sdevouard@gmail.com>
2024-08-20 11:22:51 +0200

    Support for ?cof2 to show COF2 datase

--- routes/datasets.js
+++ routes/datasets.js
@@ -6,9 +6,14 @@ const datasets = require('../datasets.json');
 
 router.get('/', (req, res, next) => {
   const includeAll = int(req.query.all);
-  const data = datasets.filter(
-    (dataset) => includeAll == 1 || !dataset.hidden || false
-  );
+  const includeCOF2 = int(req.query.cof2);
+  const data = datasets.filter((dataset) => {
+    if (includeAll === 1) return true;
+    if (includeCOF2 === 1 && dataset.dbid === "cof2") return true;
+    const hidden = dataset.hidden || false;
+    return !hidden;
+  });
+  console.log(data.map(ds => ds.dbid));
   Ok(res, data);
 });
 
